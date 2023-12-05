5dda0874bfc500b5280bf9a8be995b02f20d339d
nird <nirdiamant21@gmail.com>
2023-12-06 00:09:22 +0200

    removed text encoder lora state dict

--- sdxl_the_chosen_one.py
+++ sdxl_the_chosen_one.py
@@ -44,7 +44,9 @@ from transformers import AutoTokenizer, PretrainedConfig, CLIPTextModel, CLIPTok
 import diffusers
 from diffusers import AutoencoderKL, DDPMScheduler, StableDiffusionXLPipeline, UNet2DConditionModel
 from diffusers.loaders import LoraLoaderMixin
-from diffusers.models.lora import LoRALinearLayer, text_encoder_lora_state_dict
+# from diffusers.models.lora import LoRALinearLayer, text_encoder_lora_state_dict
+from diffusers.models.lora import LoRALinearLayer
+
 from diffusers.optimization import get_scheduler
 from diffusers.training_utils import compute_snr
 from diffusers.utils import check_min_version, is_wandb_available
